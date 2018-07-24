db4e824c01fae010fe43c4ec489643d4686bc0a2
rafael.batista <rafael.aba@outlook.com>
2018-07-24 13:10:12 -0300

    testing

--- Araujo/keep_vpn_alive.py
+++ Araujo/keep_vpn_alive.py
@@ -1,10 +1,6 @@
 from pywinauto.application import Application
-import os
 
 app = Application().connect(path=r"C:\Program Files\Palo Alto Networks\GlobalProtect\PanGPA.exe")
-hostname = "172.16.220.201"	
-while True:
-    response = os.system("ping " + hostname)
-    if response != 0:
-        app.GlobalProtect.Connect.click()
-        print ('Connecting!')
+#while True:
+for child in app.GlobalProtect.TabControl.children():
+    print(child.PrintControlIdentifiers()) # prints a dictionary
