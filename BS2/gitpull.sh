949364f3cc586d0eea2b7a47c4e2016e290309f7
rafael.batista <rafael.aba@outlook.com>
2019-06-04 16:29:01 -0300

    Ultimas alterações nos scripts para o bs2

--- BS2/gitpull.sh
+++ BS2/gitpull.sh
@@ -1,3 +1,16 @@
 #!/bin/sh
+echo 'Starting pull on repository AgendaAntecipacao'
+git -C /c/DTI/BS2/bsi.ws.agendaantecipacao/ pull
+echo 'Starting pull on repository Antecipacao - API'
 git -C /c/DTI/BS2/Antecipacao/ pull
-read -p 'Press [Enter] key to finish'
+echo 'Starting pull on repository ClienteAntecipacao'
+git -C /c/DTI/BS2/bsi.ws.clienteantecipacao/ pull
+echo 'Starting pull on repository Antecipacao - Luna/Portal'
+git -C /c/DTI/BS2/bsi.web.antecipacao/ pull
+echo 'Starting pull on repository ParametroAntecipacao'
+git -C /c/DTI/BS2/bsi.ws.parametroantecipacao/ pull
+echo 'Starting pull on repository PedidoAntecipacao'
+git -C /c/DTI/BS2/bsi.ws.pedidoantecipacao/ pull
+echo 'Starting pull on repository ProspeccaoAntecipacao'
+git -C /c/DTI/BS2/bsi.ws.prospeccaoantecipacao/ pull
+read -p 'Done. Press [Enter] key to close'
